b714860a0733d5d03ccc4a6e015a7df452c92cb3
令狐少侠 <czc12580520@gmail.com>
2021-01-10 18:18:04 +0800

    gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,27 @@
 module.exports = {
   siteMetadata: {
-    title: "gatsby-site",
+    title: 'gatsby-site',
   },
-  plugins: [],
-};
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    `gatsby-plugin-mdx`,
+    {
+      resolve: 'gatsby-plugin-eslint',
+      options: {
+        test: /\.js$|\.jsx$/,
+        exclude: /(node_modules|.cache|public)/,
+        stages: ['develop'],
+        options: {
+          emitWarning: true,
+          failOnError: false,
+        },
+      },
+    },
+  ],
+}
