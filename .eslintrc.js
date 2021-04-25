20ca43c264d53fbdbbaf5edccbf2d2bf0b2c409e
LeoKu <czc12580520@gmail.com>
2021-04-25 23:37:31 +0800

    leoku

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,16 +6,15 @@ module.exports = {
   extends: [
     'react-app',
     'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
     'plugin:react/recommended',
     'plugin:prettier/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
   ],
-  plugins: ['import', 'jsx-a11y', 'simple-import-sort'],
+  plugins: ['simple-import-sort'],
   rules: {
-    'simple-import-sort/imports': 2,
-    'simple-import-sort/exports': 2,
-    'sort-imports': 0,
-    'import/order': 0,
     'react-hooks/exhaustive-deps': 0,
     'react/prop-types': 0,
     'react/jsx-sort-props': [
@@ -29,5 +28,16 @@ module.exports = {
         reservedFirst: true,
       },
     ],
+    'simple-import-sort/imports': 2,
+    'simple-import-sort/exports': 2,
+    'sort-imports': 0,
+    'import/order': 0,
+    'import/no-unresolved': [
+      2,
+      {
+        ignore: ['^~/', '^@/'], // @ 是设置的路径别名
+      },
+    ],
+    'jsx-a11y/accessible-emoji': 0,
   },
 }
